6d1cd0bce67835d6fbc9251926693546f36c3ed7
sanjus-robotic-studio <58104863+sanjus-robotic-studio@users.noreply.github.com>
2020-01-25 17:04:13 +0530

    Add files via upload

---
+++ ARDUINO_flag_hoisting.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(12,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(11,LOW);
+delay(50);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  digitalWrite(11,LOW);
+  
+}
